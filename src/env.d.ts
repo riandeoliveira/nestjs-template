7b54ab99eb7abd1681be5c8514f56a4f35eff8e4
Rian Oliveira <riandiasdeoliveira2001@gmail.com>
2024-07-14 14:38:30 -0300

    feat: migrate database from mongodb to sqlite

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,6 +1,6 @@
 declare namespace NodeJS {
   interface ProcessEnv {
-    DATABASE_URI: string;
+    DATABASE_SOURCE: string;
     JWT_SECRET: string;
     NODE_ENV: "development" | "production";
   }
