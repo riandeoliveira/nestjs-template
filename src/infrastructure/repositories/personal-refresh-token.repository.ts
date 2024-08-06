ca34d5f49da6c47a3e354203946eb86186a5b8fa
Rian Oliveira <riandiasdeoliveira2001@gmail.com>
2024-08-06 00:55:40 -0300

    feat: improve repository methods

--- src/infrastructure/repositories/personal-refresh-token.repository.ts
+++ src/infrastructure/repositories/personal-refresh-token.repository.ts
@@ -1,27 +1,65 @@
 import { PersonalRefreshToken } from "@/domain/entities/personal-refresh-token.entity";
-import { Injectable } from "@nestjs/common";
+import { ResponseMessages } from "@/domain/enums/response-messages.enum";
+import { IRepository } from "@/domain/interfaces/repository.interface";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { Prisma } from "@prisma/client";
 import { PrismaService } from "../services/prisma.service";
 
 @Injectable()
-export class PersonalRefreshTokenRepository {
+export class PersonalRefreshTokenRepository
+  implements IRepository<PersonalRefreshToken, Prisma.PersonalRefreshTokenWhereUniqueInput>
+{
   public constructor(private readonly prisma: PrismaService) {}
 
   public async create(personalRefreshToken: PersonalRefreshToken): Promise<void> {
     await this.prisma.personalRefreshToken.create({ data: personalRefreshToken });
   }
 
-  public async findFirstWhere(
-    conditions: Prisma.PersonalRefreshTokenWhereInput,
+  public async findFirst(
+    where: Prisma.PersonalRefreshTokenWhereInput,
   ): Promise<PersonalRefreshToken> {
     return await this.prisma.personalRefreshToken.findFirst({
-      where: conditions,
+      where,
     });
   }
 
-  public async softDeleteMany(conditions: Prisma.PersonalRefreshTokenWhereInput): Promise<void> {
+  public async findOne(
+    where: Prisma.PersonalRefreshTokenWhereUniqueInput,
+  ): Promise<PersonalRefreshToken | null> {
+    return await this.prisma.personalRefreshToken.findUnique({
+      where,
+    });
+  }
+
+  public async findOneOrThrow(
+    where: Prisma.PersonalRefreshTokenWhereUniqueInput,
+  ): Promise<PersonalRefreshToken> {
+    const personalRefreshToken: PersonalRefreshToken | null = await this.findOne(where);
+
+    if (!personalRefreshToken)
+      throw new NotFoundException(ResponseMessages.PERSONAL_REFRESH_TOKEN_NOT_FOUND);
+
+    return personalRefreshToken;
+  }
+
+  public async hardDelete(where: Prisma.PersonalRefreshTokenWhereUniqueInput): Promise<void> {
+    await this.prisma.personalRefreshToken.delete({
+      where,
+    });
+  }
+
+  public async softDelete(where: Prisma.PersonalRefreshTokenWhereUniqueInput): Promise<void> {
+    await this.prisma.personalRefreshToken.update({
+      where,
+      data: {
+        deletedAt: new Date(),
+      },
+    });
+  }
+
+  public async softDeleteMany(where: Prisma.PersonalRefreshTokenWhereInput): Promise<void> {
     await this.prisma.personalRefreshToken.updateMany({
-      where: conditions,
+      where,
       data: {
         deletedAt: new Date(),
       },
@@ -29,11 +67,11 @@ export class PersonalRefreshTokenRepository {
   }
 
   public async update(
-    conditions: Prisma.PersonalRefreshTokenWhereUniqueInput,
+    where: Prisma.PersonalRefreshTokenWhereUniqueInput,
     data: Partial<PersonalRefreshToken>,
   ): Promise<void> {
     await this.prisma.personalRefreshToken.update({
-      where: conditions,
+      where,
       data: {
         updatedAt: new Date(),
         ...data,
--- src/infrastructure/repositories/user.repository.ts
+++ src/infrastructure/repositories/user.repository.ts
@@ -1,40 +1,65 @@
 import { User } from "@/domain/entities/user.entity";
-import { Injectable } from "@nestjs/common";
+import { ResponseMessages } from "@/domain/enums/response-messages.enum";
+import { IRepository } from "@/domain/interfaces/repository.interface";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { Prisma } from "@prisma/client";
 import { PrismaService } from "../services/prisma.service";
 
 @Injectable()
-export class UserRepository {
+export class UserRepository implements IRepository<User, Prisma.UserWhereUniqueInput> {
   public constructor(private readonly prisma: PrismaService) {}
 
   public async create(user: User): Promise<void> {
     await this.prisma.user.create({ data: user });
   }
 
-  public async findFirstWhere(conditions: Prisma.UserWhereInput): Promise<User> {
+  public async findFirst(where: Prisma.UserWhereInput): Promise<User> {
     return await this.prisma.user.findFirst({
-      where: conditions,
+      where,
     });
   }
 
-  public async findOneWhere(conditions: Prisma.UserWhereUniqueInput): Promise<User> {
+  public async findOne(where: Prisma.UserWhereUniqueInput): Promise<User | null> {
     return await this.prisma.user.findUnique({
-      where: conditions,
+      where,
     });
   }
 
-  public async softDelete(conditions: Prisma.UserWhereUniqueInput): Promise<void> {
+  public async findOneOrThrow(where: Prisma.UserWhereUniqueInput): Promise<User> {
+    const user: User | null = await this.findOne(where);
+
+    if (!user) throw new NotFoundException(ResponseMessages.USER_NOT_FOUND);
+
+    return user;
+  }
+
+  public async hardDelete(where: Prisma.UserWhereUniqueInput): Promise<void> {
+    await this.prisma.user.delete({
+      where,
+    });
+  }
+
+  public async softDelete(where: Prisma.UserWhereUniqueInput): Promise<void> {
     await this.prisma.user.update({
-      where: conditions,
+      where,
       data: {
         deletedAt: new Date(),
       },
     });
   }
 
-  public async update(conditions: Prisma.UserWhereUniqueInput, data: Partial<User>): Promise<void> {
+  public async softDeleteMany(where: Prisma.UserWhereInput): Promise<void> {
+    await this.prisma.user.updateMany({
+      where,
+      data: {
+        deletedAt: new Date(),
+      },
+    });
+  }
+
+  public async update(where: Prisma.UserWhereUniqueInput, data: Partial<User>): Promise<void> {
     await this.prisma.user.update({
-      where: conditions,
+      where,
       data: {
         updatedAt: new Date(),
         ...data,
