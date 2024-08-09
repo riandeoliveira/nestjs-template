b1551289db765907f61140af0e011205cf669e6d
Rian Oliveira <riandiasdeoliveira2001@gmail.com>
2024-08-09 11:52:26 -0300

    fix: fix web application builder types

--- src/web-api/builders/web-application.builder.ts
+++ src/web-api/builders/web-application.builder.ts
@@ -1,15 +1,16 @@
-import { INestApplication } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
+import { NestExpressApplication } from "@nestjs/platform-express";
+import { IncomingMessage, Server, ServerResponse } from "http";
 import { DocumentationExtension } from "../extensions/documentation.extension";
 import { ProblemDetailsExtension } from "../extensions/problem-details.extension";
 import { ValidationExtension } from "../extensions/validation.extension";
 import { WebApiModule } from "../web-api.module";
 
 export class WebApplicationBuilder {
-  public application: INestApplication;
+  public application: NestExpressApplication<Server<typeof IncomingMessage, typeof ServerResponse>>;
 
   public async create(): Promise<void> {
-    this.application = await NestFactory.create(WebApiModule);
+    this.application = await NestFactory.create<NestExpressApplication>(WebApiModule);
   }
 
   public async run(): Promise<void> {
