e6e424f122c03c7af8bb29e8ab9aa6699375aa05
Rian Oliveira <riandiasdeoliveira2001@gmail.com>
2024-08-09 11:53:19 -0300

    fix: fix jwt secret key error

--- src/infrastructure/modules/auth/auth.module.ts
+++ src/infrastructure/modules/auth/auth.module.ts
@@ -1,9 +1,11 @@
 import { Module } from "@nestjs/common";
 import { JwtModule } from "@nestjs/jwt";
+import { EnvironmentModule } from "../environment/environment.module";
 import { AuthService } from "./auth.service";
 
 @Module({
   imports: [
+    EnvironmentModule,
     JwtModule.register({
       global: true,
       secret: process.env.JWT_SECRET,
