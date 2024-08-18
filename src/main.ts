fd0c70c1e40005e3048f731bd375a13c28ad9e46
Rian Oliveira <riandiasdeoliveira2001@gmail.com>
2024-08-18 16:44:50 -0300

    .

--- src/main.ts
+++ src/main.ts
@@ -11,8 +11,6 @@ import { WebApplicationBuilder } from "./web-api/builders/web-application.builde
   // builder.application.use(cookieParser());
 
   builder.application.enableCors({
-    credentials: true,
-    methods: "GET,POST,PUT,DELETE,PATCH,OPTIONS",
     origin: "*",
   });
 
